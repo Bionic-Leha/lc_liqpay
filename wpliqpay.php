088d23a2e22cbe434b43d1637b916ba27e8d9074
Bionic Leha <bionic.mods@gmail.com>
2017-04-22 14:34:52 +0300

    Creating tables on init plugin and saving token to database after generate

--- wpliqpay.php
+++ wpliqpay.php
@@ -15,6 +15,24 @@ define('BUYLP_URL', plugin_dir_url(__FILE__));
 
 require(BUYLP_DIR . 'LiqPay.php');
 
+register_activation_hook(__FILE__, 'mainInit');
+function mainInit() {
+    global $wpdb;
+    $wpdb->get_var("CREATE TABLE IF NOT EXISTS lp_dload(
+    token TEXT NOT NULL,
+    expiry INT,
+    pay_status TEXT,
+    user_sig TEXT,
+    lp_sig TEXT,
+    active INT DEFAULT 0,
+    buy_date TEXT NULL
+    )");
+
+    $wpdb->get_var("CREATE TABLE IF NOT EXISTS lp_conf(
+    file_link TEXT,
+    expiry_period INT,
+    )");
+}
 
 function readPostData()
 {
@@ -48,9 +66,9 @@ function liqpay_notice()
     $user_count = $wpdb->get_var("SELECT COUNT(*) FROM $wpdb->users");
     $public_key = "i83816479078";
     $private_key = "8zmMxw0qJLPHCRPc2c1lkYU4OalUEASGS4i4DaJU";
-
+    isset($_COOKIE["download_token"]) ? $token = $_COOKIE["download_token"] : $token = generateLink();
     $liqpay = new LiqPay($public_key, $private_key);
-    $html = $liqpay->cnb_form(array(
+    $lp_data = array(
         'action' => 'pay',
         'amount' => '1',
         'currency' => 'USD',
@@ -62,12 +80,15 @@ function liqpay_notice()
         'result_url' => 'https://itstest.ml',
         'verifycode' => 'Y',
         'info' => '22814881997'
-    ));
-    // init
+    );
+    $html = $liqpay->cnb_form($lp_data);
+    $calc_sign = base64_encode(sha1($private_key.$lp_data.$private_key, 1));
+    $wpdb->get_var("SELECT COUNT(*) FROM $wpdb->users");
     print("<div id='liqpay-test' style='float: right;'>
                <br>Plugin url: " . BUYLP_URL . "<br>Plugin dir: " . BUYLP_DIR . "<br>
+               User count: {$user_count}<br>
                {$html}<br>
-               Test token: " . generateLink() . "
+               Test token: {$token}
            </div>");
 }
 add_action('wp_print_scripts', 'liqpay_notice');
@@ -75,11 +96,14 @@ add_action('wp_print_scripts', 'liqpay_notice');
 // test
 
 function generateLink(){
+    global $wpdb;
     $chars = 'qwertyuiopasdfghjklzxcvbnm1234567890QAZWSXEDCRFVTGBYHNUJMIKOLP';
     $numChars = strlen($chars);
     $string = '';
     for ($i = 0; $i < 30; $i++) {
         $string .= substr($chars, rand(1, $numChars) - 1, 1);
     }
+    $wpdb->get_var("INSERT INTO lp_dload (token) VALUE ('{$string}')");
+    setcookie("download_token", $string,time()+3600);
     return $string;
 }
